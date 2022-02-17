937298de3834fda2fdb012014566deb805835600
FOMonti <montorofernando4315@gmail.com>
2022-02-17 19:16:47 -0300

    Usuario-Login-Registrarme

--- ep_movil/src/main/java/com/ep_movil/controladores/UsuarioController.java
+++ ep_movil/src/main/java/com/ep_movil/controladores/UsuarioController.java
@@ -5,6 +5,7 @@ import com.ep_movil.servicios.UsuarioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
@@ -14,17 +15,31 @@ public class UsuarioController {
     @Autowired
     private UsuarioServiceImpl usuarioService;
 
-    @GetMapping("/crear")
+    @GetMapping("/registrar")
     public String toCreate(Model model) {
         model.addAttribute("titulo", "Crear Usuario");
         model.addAttribute("usuario", new Usuario());
         return "usuario-form";
     }
 
-    @GetMapping("/login")
+    @GetMapping("/logear")
+    public String toLog(Model model) {
+        model.addAttribute("titulo", "Logeando");
+        model.addAttribute("usuario", new Usuario());
+        return "usuario-login";
+    }
+
+    @PostMapping("/login")
     public String toLogin(Model model) {
         model.addAttribute("titulo", "Crear Usuario");
         model.addAttribute("usuario", new Usuario());
-        return "usuario-login";
+        return "redirect:/usuario-login";
+    }
+
+    @PostMapping("/registro")
+    public String guardarUsuarios(/*@RequestParam(name = "file", required = false) MultipartFile portada,*/
+            Model model, Usuario usuario) { //RedirectAttributes redirect / Model model
+        usuarioService.guardarUsuario(usuario);
+        return "redirect:/";
     }
 }
