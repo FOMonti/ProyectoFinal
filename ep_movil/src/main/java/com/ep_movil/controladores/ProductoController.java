9b71c700f068aecb9467764e1ccf8c6a087618d0
FOMonti <montorofernando4315@gmail.com>
2022-02-17 20:53:32 -0300

    Boton modificar

--- ep_movil/src/main/java/com/ep_movil/controladores/ProductoController.java
+++ ep_movil/src/main/java/com/ep_movil/controladores/ProductoController.java
@@ -8,10 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -36,9 +33,4 @@ public class ProductoController {
         return "/admin/productoForm";
     }
 
-
-//    @GetMapping("/modificar{idProducto}")
-//    public String modificarProducto(Producto producto){
-//        productoService.encontrarProducto(producto);
-//        return "redirect:/";
 }
