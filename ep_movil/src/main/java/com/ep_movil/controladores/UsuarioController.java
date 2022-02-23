0ad0219991a60cf4c31ac048e11835d1354cedde
FOMonti <montorofernando4315@gmail.com>
2022-02-23 20:34:32 -0300

    Carrito Vista-Correcion de Paginacion

--- ep_movil/src/main/java/com/ep_movil/controladores/UsuarioController.java
+++ ep_movil/src/main/java/com/ep_movil/controladores/UsuarioController.java
@@ -1,6 +1,8 @@
 package com.ep_movil.controladores;
 
+import com.ep_movil.entidades.Producto;
 import com.ep_movil.entidades.Usuario;
+import com.ep_movil.servicios.IProductoService;
 import com.ep_movil.servicios.UsuarioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,12 +10,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping({"/usuario"})
 public class UsuarioController {
+
     @Autowired
     private UsuarioServiceImpl usuarioService;
+
+    @Autowired
+    private IProductoService productoService;
 
     @GetMapping("/registrar")
     public String toCreate(Model model) {
@@ -38,8 +45,35 @@ public class UsuarioController {
 
     @PostMapping("/registro")
     public String guardarUsuarios(/*@RequestParam(name = "file", required = false) MultipartFile portada,*/
-            Model model, Usuario usuario) { //RedirectAttributes redirect / Model model
+            Model model, Usuario usuario, RedirectAttributes redirect) { //RedirectAttributes redirect / Model model
         usuarioService.guardarUsuario(usuario);
+        redirect.addAttribute("usuario", usuario);
         return "redirect:/";
     }
+
+    @GetMapping("/carrito")
+    public String toCarrito(Model model, Usuario usuario) {
+/*  Si el usuario no esta logeado...?
+ if (usuario.equals(null)) {
+     return "redirect:/";
+ }
+*/
+        model.addAttribute("list", productoService.listarProductos());
+        model.addAttribute("titulo", "Carrito");
+        model.addAttribute("usuario", usuario);
+        return "/user/carrito";
+    }
+//    @GetMapping("/carrito")
+//    public String toCarrito(Model model, Usuario usuario) {
+///*  Si el usuario no esta logeado...?
+// if (usuario.equals(null)) {
+//     return "redirect:/";
+// }
+//*/
+
+//        model.addAttribute("carrito", productoService.findByUsuarioId(usuario.id));
+//        model.addAttribute("titulo", "Mostrando Carrito");
+//        model.addAttribute("usuario", usuario);
+//        return "/user/carrito";
+//    }
 }
